f5e8ef83d4a55b68927ac6d827c04d42b3b2e30a
Dauda Abdulrahman Gaya <57319135+DoudGaya@users.noreply.github.com>
2021-11-14 12:23:26 +0100

    store-ui no js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'profile': ['Bebas Neue', 'cursive'],
+        'logo' : ['Sacramento']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
